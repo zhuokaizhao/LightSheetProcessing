0eb0a5ea7747a5dd53535723f58359e98a44ebaf
zhuokaizhao <zhuokai@uchicago.edu>
2018-12-17 10:45:24 -0800

    integrated start.cpp

--- include/proj.h
+++ include/proj.h
@@ -13,7 +13,7 @@
 struct projOptions {
 	int file_number = 0;
 	std::string nhdr_path;
-    std::string czi_path;
+    //std::string czi_path;
 	std::string proj_path;
     // we no longer need base name
     //std::string base_name;
