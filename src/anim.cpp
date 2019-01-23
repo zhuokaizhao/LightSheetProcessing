dac66ff03e6f17e47bdfc588031627e1d4fd5eba
zhuokaizhao <zhuokai@uchicago.edu>
2019-01-23 11:45:29 -0600

    redo the resampling part in anim.cpp

--- src/anim.cpp
+++ src/anim.cpp
@@ -341,18 +341,18 @@ void Anim::split_type()
             float x0 = origins[i][0], y0 = origins[i][1], z0 = origins[i][2];
             cout << "New origin is: " << "(" << x0 << ", " << y0 << ", " << z0 << ")" << endl;
 
-            // get min and max values for each dimension
-            int minx = minmax[0][0], maxx = minmax[0][1];
-            cout << "minx = " << minx << endl;
-            cout << "maxx = " << maxx << endl;
+            // get min and max origins for each dimension
+            int x0_min = minmax[0][0], x0_max = minmax[0][1];
+            cout << "minx = " << x0_min << endl;
+            cout << "maxx = " << x0_max << endl;
 
-            int miny = minmax[1][0], maxy = minmax[1][1];
-            cout << "miny = " << miny << endl;
-            cout << "maxy = " << maxy << endl;
+            int y0_min = minmax[1][0], y0_max = minmax[1][1];
+            cout << "miny = " << y0_min << endl;
+            cout << "maxy = " << y0_max << endl;
 
-            int minz = minmax[2][0], maxz = minmax[2][1];
-            cout << "minz = " << minz << endl;
-            cout << "maxz = " << maxz << endl;
+            int z0_min = minmax[2][0], z0_max = minmax[2][1];
+            cout << "minz = " << z0_min << endl;
+            cout << "maxz = " << z0_max << endl;
             
 
             // take off the cropping part after discussing with Gordon
