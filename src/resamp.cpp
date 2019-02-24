b74296ff59acd20ec7a94e35d2f7445275332155
zhuokaizhao <zhuokai@uchicago.edu>
2019-02-24 17:39:47 -0600

    debugging

--- src/resamp.cpp
+++ src/resamp.cpp
@@ -372,10 +372,7 @@ int nrrdResample3D(lspVolume* newVolume, lspCtx3D* ctx3D)
     for (uint zi = 0; zi < sizeZ; zi++)
     {
         double percentage = 100.0*(double)zi/(double)sizeZ;
-        if (opt.verbose)
-        {
-            cout << percentage << " percents of the volume have been done" << endl;
-        }
+        // cout << percentage << " percents of the volume have been done" << endl;
 
         for (uint yi = 0; yi < sizeY; yi++)
         {
