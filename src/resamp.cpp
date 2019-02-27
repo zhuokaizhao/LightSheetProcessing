ff82c47ec4e13ff9641561c3bd748ce2dc35fb5d
zhuokaizhao <zhuokai@uchicago.edu>
2019-02-27 15:27:52 -0600

    added single mode

--- src/resamp.cpp
+++ src/resamp.cpp
@@ -146,11 +146,8 @@ void setup_resamp(CLI::App &app)
         {
             // the program also handles if input file is a single file
             opt->isSingleFile = true;
-
-            cout << opt->nhdr_path << " is not a directory, check if it is a valid .nhdr file" << endl;
             const string curFile = opt->nhdr_path;
-
-            std::cout << "Current file name is: " << curFile << endl;
+            cout << curFile << " is not a directory, check if it is a valid .nhdr file" << endl;
             
             // check if input file is a .nhdr file
             int suff = curFile.rfind(".nhdr");
@@ -163,14 +160,19 @@ void setup_resamp(CLI::App &app)
             else
             {
                 cout << "Current input file " + curFile + " ends with .nhdr, process this file" << endl;
+                // we want to actually learn the name of the file with .nhdr
+                int start = curFile.rfind("/");
+                int end = curFile.rfind(".nhdr");
+                string curFileName = curFile.substr(start, end);
+                std::cout << "Current file name is: " << curFileName << endl;
 
                 try
                 {
-                    // we will save this volume as nrrd
-                    string volumeOutPath = opt->out_path + ".nhdr";
+                    // we will save this new volume as nrrd
+                    string volumeOutPath = opt->out_path + "/" + curFileName + ".nhdr";
 
                     // we will save the final nrrd as image
-                    string imageOutPath = opt->out_path + ".png";
+                    string imageOutPath = opt->out_path + "/" + curFileName + ".png";
 
                     // when output already exists, skip this
                     if (fs::exists(volumeOutPath) && fs::exists(imageOutPath))
@@ -185,6 +187,7 @@ void setup_resamp(CLI::App &app)
                     auto stop = chrono::high_resolution_clock::now(); 
                     auto duration = chrono::duration_cast<chrono::seconds>(stop - start); 
                     cout << endl << "Processed " << opt->nhdr_path << " took " << duration.count() << " seconds" << endl << endl; 
+                    // we don't make video here since it is just a single image
                 }
                 catch(LSPException &e)
                 {
@@ -192,7 +195,6 @@ void setup_resamp(CLI::App &app)
                 }
             }
         }
-        
     });
 }
 
@@ -673,10 +675,229 @@ void Resamp::main()
     }
     else
     {
-        // to be continued
+        auto start = chrono::high_resolution_clock::now();
+
+        // since it is single file mode, nhdr_path is now the file path
+        const string nhdr_name = opt->nhdr_path;
+        // get the name of the file only
+        int start = nhdr_name.rfind("/");
+        int end = nhdr_name.rfind(".nhdr");
+        string curFileName = nhdr_name.substr(start, end);
+
+        // we will save this new volume as nrrd
+        string volumeOutPath = opt->out_path + "/" + curFileName + ".nhdr";
+        // we will save the final nrrd as image
+        string imageOutPath = opt->out_path + "/" + curFileName + ".png";
+
+        // load the nhdr header
+        Nrrd* nin = safe_nrrd_load(mop, nhdr_name);
+        if (opt.verbose)
+        {
+            cout << "Finish loading Nrrd data located at " << nhdr_name << endl;
+        }
+
+        // do the permutation
+        // permute from x(0)-y(1)-channel(2)-z(3) to channel(2)-x(0)-y(1)-z(3)
+        unsigned int permute[4] = {2, 0, 1, 3};
+        Nrrd* nin_permuted = safe_nrrd_new(mop, (airMopper)nrrdNuke);
+        if ( nrrdAxesPermute(nin_permuted, nin, permute) )
+        {
+            if (opt.verbose)
+            {
+                cout << "nrrdAxesPermute failed, program stops" << endl;
+            }
+            return;
+        }
+        if (opt.verbose)
+        {
+            cout << "Finished permutation" << endl;
+        }
+
+        // put Nrrd data into lspVolume
+        lspVolume* volume = lspVolumeNew();
+        // airMopAdd(mop, volume, (airMopper)lspVolumeNix, airMopAlways);
+
+        if ( lspVolumeFromNrrd(volume, nin_permuted) )
+        {
+            if (opt.verbose)
+            {
+                cout << "lspVolumeFromNrrd failed, program stops" << endl;
+            }
+            return;
+        }
+        if (opt.verbose)
+        {
+            cout << "Finished converting Nrrd data to lspVolume" << endl;
+        }
+
+        // put both volume and user-defined kernel into the Ctx3D
+        const NrrdKernel* kernel;
+        if (opt.kernel_name == "box")
+        {
+            kernel = nrrdKernelBox;
+        }
+        else if (opt.kernel_name == "ctml")
+        {
+            kernel = nrrdKernelCatmullRom;
+        }
+
+        // get the container ready
+        lspCtx3D* ctx = lspCtx3DNew(volume, opt.grid_path, kernel, NULL);
+        if (opt.verbose)
+        {
+            cout << "Finished generating ctx container" << endl;
+        }
+
+        // perform the 3D sampling (convolution)
+        lspVolume* volume_new = lspVolumeNew();
+        // airMopAdd(mop, volume_new, (airMopper)lspVolumeNix, airMopAlways);
+
+        // allocate memory for the new volume, with sizes being the region of interest sizes
+        if (lspVolumeAlloc(volume_new, volume->channel, ctx->boundaries[0], ctx->boundaries[1], ctx->boundaries[2], volume->dtype)) 
+        {
+            if (opt.verbose)
+            {
+                printf("%s: trouble allocating volume\n", __func__);
+            }
+            return;
+        }
+
+        // start 3D convolution
+        if ( nrrdResample3D(volume_new, ctx) )
+        {
+            if (opt.verbose)
+            {
+                printf("%s: trouble computing 3D convolution\n", __func__);
+            }
+            return;
+        }
+        if (opt.verbose)
+        {
+            cout << "Finished resampling with Box kernel" << endl;
+        }
+
+        // change the volume back to Nrrd file for projection
+        Nrrd* nrrd_new = safe_nrrd_new(mop, (airMopper)nrrdNuke);
+        if (lspNrrdFromVolume(nrrd_new, volume_new))
+        {
+            if (opt.verbose)
+            {
+                printf("%s: trouble converting Volume to Nrrd data\n", __func__);
+            }
+            return;
+        }
+        if (opt.verbose)
+        {
+            cout << "Finished converting resulting volume back to Nrrd data" << endl;
+        }
+
+        if (nrrdSave(volumeOutPath.c_str(), nrrd_new, NULL)) 
+        {
+            if (opt.verbose)
+            {
+                printf("%s: trouble saving new volume as Nrrd file\n", __func__);
+            }
+            airMopError(mop);
+            return;
+        }
+        cout << "Finished saving new volume at " << volumeOutPath << endl;
+
+        // Project the volume (in nrrd format) alone z axis using MIP
+        Nrrd* projNrrd = safe_nrrd_new(mop, (airMopper)nrrdNuke);
+        if (nrrdProject(projNrrd, nrrd_new, 3, nrrdMeasureMax, nrrdTypeDouble))
+        {
+            if (opt.verbose)
+            {
+                printf("%s: trouble projecting Nrrd data alone z-axis using MIP\n", __func__);
+            }
+            airMopError(mop);
+            return;
+        }
+        if (opt.verbose)
+        {
+            cout << "Finished projecting Nrrd data alone z-axis" << endl;
+        }
+
+        // slice the nrrd into separate GFP and RFP channel (and quantize to 8bit)
+        Nrrd* slices[2] = {safe_nrrd_new(mop, (airMopper)nrrdNuke), 
+                            safe_nrrd_new(mop, (airMopper)nrrdNuke)};
+
+        // quantized
+        Nrrd* quantized[2] = {safe_nrrd_new(mop, (airMopper)nrrdNuke),
+                                safe_nrrd_new(mop, (airMopper)nrrdNuke)};
+
+        // range during quantizing
+        auto range = nrrdRangeNew(lspNan(0), lspNan(0));
+        airMopAdd(mop, range, (airMopper)nrrdRangeNix, airMopAlways);
+
+        for (int i = 0; i < 2; i++)
+        {
+            if (nrrdSlice(slices[i], projNrrd, 0, i))
+            {
+                if (opt.verbose)
+                {
+                    printf("%s: trouble slicing into 2 channels\n", __func__);
+                }
+                airMopError(mop);
+            }
+            if (nrrdRangePercentileFromStringSet(range, slices[i],  "0.1%", "0.1%", 5000, true)
+                || nrrdQuantize(quantized[i], slices[i], range, 8))
+            {
+                if (opt.verbose)
+                {
+                    printf("%s: trouble quantizing to 8 bits\n", __func__);
+                }
+                airMopError(mop);
+            }
+        }
+        if (opt.verbose)
+        {
+            cout << "Finished slicing the data based on its channel (GFP and RFP)" << endl;
+            cout << "Finished quantizing to 8-bit" << endl;
+        }
+
+        // Join the two channel
+        Nrrd* finalJoined = safe_nrrd_new(mop, (airMopper)nrrdNuke);
+        if (nrrdJoin(finalJoined, quantized, 2, 0, 1))
+        {
+            if (opt.verbose)
+            {
+                printf("%s: trouble joining 2 channels\n", __func__);
+            }
+            airMopError(mop);
+        }
+        if (opt.verbose)
+        {
+            cout << "Fnished joining the 2 channels" << endl;
+        }
+
+        // right now it is two channel png [GFP RFP], we want to make it a three channel [RFP GFP RFP]
+        // unu pad -i 598.png -min -1 0 0 -max M M M -b wrap -o tmp.png
+        Nrrd* finalPaded = safe_nrrd_new(mop, (airMopper)nrrdNuke);
+        ptrdiff_t min[3] = {-1, 0, 0};
+        ptrdiff_t max[3] = {(ptrdiff_t)finalJoined->axis[0].size-1, (ptrdiff_t)finalJoined->axis[1].size-1, (ptrdiff_t)finalJoined->axis[2].size-1};
+        nrrdPad_va(finalPaded, finalJoined, min, max, nrrdBoundaryWrap);
+
+        if (nrrdSave(imageOutPath.c_str(), finalPaded, NULL)) 
+        {
+            if (opt.verbose)
+            {
+                printf("%s: trouble saving output\n", __func__);
+            }
+            airMopError(mop);
+        }
+        cout << "Finished saving image at " << imageOutPath << endl;
+
+        // free memory
+        lspVolumeNix(volume);
+        lspVolumeNix(volume_new);
+        lspCtx3DNix(ctx);
+
+        auto stop = chrono::high_resolution_clock::now(); 
+        auto duration = chrono::duration_cast<chrono::seconds>(stop - start); 
+        cout << endl << "Processed " << nhdr_name << " took " << duration.count() << " seconds" << endl << endl; 
     }
-    
-    
+
     return;
 }
 
