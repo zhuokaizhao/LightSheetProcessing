efb1174c28abf8ddfc6379ed7fbe24ea9cf94e6d
zhuokaizhao <zhuokai@uchicago.edu>
2019-02-28 20:05:31 -0600

    added free back

--- scripts/run_single.sh
+++ scripts/run_single.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for ((number = 100; number<=598; number++))
 do
-	lsp resamp -i corr_nhdr/$number.nhdr -g grid.txt -k ctml -o resamp -v 0
+	lsp resamp -i corr_nhdr/$number.nhdr -g grid.txt -k ctml -o resamp -v 1
 done
 
 
