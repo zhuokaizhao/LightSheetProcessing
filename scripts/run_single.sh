7656eca35e56d7399be63344e1d37a6b4aa338e0
zhuokaizhao <zhuokai@uchicago.edu>
2019-03-03 20:06:03 -0600

    updated sh

--- scripts/run_single.sh
+++ scripts/run_single.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for ((number = 1; number<=9; number++))
 do
-	lsp resamp -i corr_nhdr/00$number.nhdr -g grid.txt -k ctml -o resamp -v 1
+	lsp resamp -i corr_nhdr/00$number.nhdr -g grid.txt -k ctml -o resamp_ctml -v 1
 done
 
 
