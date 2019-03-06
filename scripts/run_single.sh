5c5c38a4f1744734cfe10dcf0a5ab01762303a32
zhuokaizhao <zhuokai@uchicago.edu>
2019-03-06 15:17:59 -0600

    changed sh

--- scripts/run_single.sh
+++ scripts/run_single.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-for ((number = 46; number<=99; number++))
+for ((number = 1; number<=9; number++))
+do
+	lsp resamp -i resamp_box/00$number.nhdr -m VideoOnly -g grid.txt -k box -o resamp3D -v 1
+done
+for ((number = 10; number<=99; number++))
 do
 	lsp resamp -i resamp_box/0$number.nhdr -m VideoOnly -g grid.txt -k box -o resamp3D -v 1
 done
