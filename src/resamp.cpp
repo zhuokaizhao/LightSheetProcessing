273462ab469db589e961fb2139ef060debab9965
zhuokaizhao <zhuokai@uchicago.edu>
2019-03-06 11:18:12 -0600

    changed ranges

--- src/resamp.cpp
+++ src/resamp.cpp
@@ -910,9 +910,9 @@ void Resamp::main()
             }
 
             // min percentile for GFP and RFP in quantization
-            vector<string> rangeMinPercentile = {"0.1%", "10.01%"};
+            vector<string> rangeMinPercentile = {"0.1%", "5.01%"};
             // max percentile for GFP and RFP in quantization
-            vector<string> rangeMaxPercentile = {"0.1%", "10.01%"};
+            vector<string> rangeMaxPercentile = {"0.1%", "5.01%"};
             // *********************** alone x-axis ******************************
             // left
             makeProjImage(nin, "x", 0.0, 0.5, imageOutPath_x_left, rangeMinPercentile, rangeMaxPercentile, opt.verbose, mop);
