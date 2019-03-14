2c8e0ad3bf97893c4329739f411b4e7c7d368024
zhuokaizhao <zhuokai@uchicago.edu>
2019-03-14 16:47:11 -0500

    changed batch

--- scripts/run_single.sh
+++ scripts/run_single.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 for ((number = 1; number<=9; number++))
 do
-	lsp resamp -i resamp_box/00$number.nhdr -m VideoOnly -g grid.txt -k box -o resamp3D_v3 -v 0
+	lsp resamp -i resamp_box/00$number.nhdr -m VideoOnly -g grid.txt -k box -o resamp3D_v4 -v 0
 done
 for ((number = 10; number<=99; number++))
 do
-	lsp resamp -i resamp_box/0$number.nhdr -m VideoOnly -g grid.txt -k box -o resamp3D_v3 -v 0
+	lsp resamp -i resamp_box/0$number.nhdr -m VideoOnly -g grid.txt -k box -o resamp3D_v4 -v 0
 done
 for ((number = 100; number<=598; number++))
 do
-	lsp resamp -i resamp_box/$number.nhdr -m VideoOnly -g grid.txt -k box -o resamp3D_v3 -v 0
+	lsp resamp -i resamp_box/$number.nhdr -m VideoOnly -g grid.txt -k box -o resamp3D_v4 -v 0
 done
-lsp resamp -i resamp_box/ -m VideoOnly -g grid.txt -k box -o resamp3D_v3 -v 0
+lsp resamp -i resamp_box/ -m VideoOnly -g grid.txt -k box -o resamp3D_v4 -v 0
 
 
 
